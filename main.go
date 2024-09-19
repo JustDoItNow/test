461aad6b9e7f2ead91f653b15c84ab39f366ecdf
JustDoItNow <coffeeball@163.com>
2024-09-19 16:59:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
   "fmt"
 )
 
-var count int
+// define count in init.go
+// var count int
 
 // init() function is a special function that is automatically called by the Go runtime before the main() function. 
 // It is used to perform initialization tasks such as setting up global variables, initializing data structures, 
@@ -17,3 +18,9 @@ func init() {
 func main() {
   fmt.Println("count value: ", count)
 }
+
+// output:
+// $ go run .
+// init() function is called: count is initilized to  100
+// init() function is called: count is initilized to  1
+// count value:  1
